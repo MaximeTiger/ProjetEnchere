69793122fcb394aff97684f84a3e0df4c8504dcb
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-06-27 17:21:55 +0200

    Delete UtilisateursManager.java

--- Enchere/src/main/java/fr/eni/enchere/bll/UtilisateursManager.java
+++
@@ -1,8 +0,0 @@
-package fr.eni.enchere.bll;
-
-import fr.eni.enchere.bo.Utilisateurs;
-
-public interface UtilisateursManager {
-    void suscribe(Utilisateurs u) throws BLLException;
-    Utilisateurs connexion(String pseudo, String motDePasse) throws  BLLException;
-}
