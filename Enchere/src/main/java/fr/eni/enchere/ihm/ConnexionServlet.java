b63bbba5dedf88005d256832e9f26720a65e72d9
MaximeTiger <maxime.tiger2022@campus-eni.fr>
2022-06-27 13:51:59 +0200

    Servlet connexion

---
+++ Enchere/src/main/java/fr/eni/enchere/ihm/ConnexionServlet.java
@@ -0,0 +1,17 @@
+package fr.eni.enchere.ihm;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/connexion")
+public class ConnexionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/connexion.jsp").forward(req,resp);
+    }
+}
