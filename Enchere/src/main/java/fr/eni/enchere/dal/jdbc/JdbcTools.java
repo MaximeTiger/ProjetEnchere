bbca9130cc07d418e6e3ddf83bbbca29eab659dd
MarionBeaupied <108260585+MarionBeaupied@users.noreply.github.com>
2022-06-27 12:29:55 +0200

    couche dal

--- Enchere/src/main/java/fr/eni/enchere/dal/jdbc/JdbcTools.java
+++ Enchere/src/main/java/fr/eni/enchere/dal/jdbc/JdbcTools.java
@@ -1,4 +1,29 @@
 package fr.eni.enchere.dal.jdbc;
 
+import fr.eni.enchere.dal.Settings;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcTools {
+
+    private static String urldb;
+    private static String userdb;
+    private static String passworddb;
+
+    static {
+        urldb = Settings.getProperty("urldb");
+        userdb = Settings.getProperty("userdb");
+        passworddb = Settings.getProperty("passworddb");
+
+        System.out.println("urldb = " + urldb);
+        System.out.println("userdb = " + userdb);
+        System.out.println("passworddb = " + passworddb);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(urldb,userdb,passworddb);
+    }
+
 }
