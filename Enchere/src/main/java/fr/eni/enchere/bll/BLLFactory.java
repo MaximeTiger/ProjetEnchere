042547ed69ab1487b3974d95fb656875afd9c8b8
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-06-27 17:21:42 +0200

    Update BLLFactory.java

--- Enchere/src/main/java/fr/eni/enchere/bll/BLLFactory.java
+++ Enchere/src/main/java/fr/eni/enchere/bll/BLLFactory.java
@@ -1,5 +1,8 @@
 package fr.eni.enchere.bll;
 
+import fr.eni.enchere.bll.utilisateurs.UtilisateursManager;
+import fr.eni.enchere.bll.utilisateurs.UtilisateursManagerImpl;
+
 public class BLLFactory {
     public static UtilisateursManager getUtilisateursManager() {return new UtilisateursManagerImpl();}
 
