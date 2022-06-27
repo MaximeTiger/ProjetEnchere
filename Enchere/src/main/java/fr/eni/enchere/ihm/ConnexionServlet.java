cf311212a54e1df28d03988c6a32976bed4ee432
MaximeTiger <maxime.tiger2022@campus-eni.fr>
2022-06-27 15:03:48 +0200

    Update ConnexionServlet.java

--- Enchere/src/main/java/fr/eni/enchere/ihm/ConnexionServlet.java
+++ Enchere/src/main/java/fr/eni/enchere/ihm/ConnexionServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/connexion")
@@ -14,5 +15,12 @@ public class ConnexionServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/pages/connexion.jsp").forward(req,resp);
     }
-    
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+
+    }
+
 }
