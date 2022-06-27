ffb9a945b2badf057a2b4cfc641ce24f8356bb76
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-06-27 14:33:07 +0200

    couche bll structure

---
+++ Enchere/src/main/java/fr/eni/enchere/bll/BLLException.java
@@ -0,0 +1,19 @@
+package fr.eni.enchere.bll;
+
+public class BLLException extends Exception  {
+
+
+        public BLLException() {
+        }
+
+        public BLLException(String message) {
+            super(message);
+        }
+
+        public BLLException(String message, Throwable exc) {
+            super(message, exc);
+        }
+
+
+
+}
