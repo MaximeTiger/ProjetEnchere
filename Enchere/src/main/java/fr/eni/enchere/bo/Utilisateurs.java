013d8330dd648bb30abd40b998adda36b59a2d7c
bryanmetro <108260564+bryanmetro@users.noreply.github.com>
2022-06-27 16:20:07 +0200

    Update Utilisateurs.java

--- Enchere/src/main/java/fr/eni/enchere/bo/Utilisateurs.java
+++ Enchere/src/main/java/fr/eni/enchere/bo/Utilisateurs.java
@@ -30,6 +30,20 @@ public class Utilisateurs {
         this.administrateur = administrateur;
     }
 
+    public Utilisateurs(String pseudo, String nom, String prenom, String email, String telephone, String rue, String codePostal, String ville, String motDePasse, String credit, boolean administrateur) {
+        this.pseudo = pseudo;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.rue = rue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+        this.motDePasse = motDePasse;
+        this.credit = credit;
+        this.administrateur = administrateur;
+    }
+
     public Utilisateurs(String pseudo, String motDePasse) {
         this.pseudo = pseudo;
         this.motDePasse = motDePasse;
